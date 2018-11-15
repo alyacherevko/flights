2827b664ac975f38f17fd085cab31fce7d47ce3c
 Alya Cherevko <alcherevko@MacBook-Air-Alya.local>
2018-11-15 18:49:11 +0300

    the script is made to display the data in the table

--- script.js
+++ script.js
@@ -15,6 +15,37 @@ request.onload = function() {
 
 
 
+function showFlightsCard(jsonObj) {
+	var flying = jsonObj["flying"];
 
+
+	for (var i = 0; i < flying.length; i++) { 
+		var card = document.createElement('div');
+		var cardList = document.createElement('ul');
+
+		var itemCardList = flying[i];
+		
+		Object.keys(itemCardList).forEach(function (key) {
+
+			if (key === "type" || key === "delay") {
+				return;
+			}
+
+  			var itemList = document.createElement('li');
+
+  			itemList.textContent = itemCardList[key];
+  			cardList.appendChild(itemList);
+  			itemList.classList.add("card__content-item");
+		});
+		
+		cardList.classList.add("card__content-list");
+
+
+		card.appendChild(cardList);
+
+		main.appendChild(card);
+
+	}
+}
 
 
